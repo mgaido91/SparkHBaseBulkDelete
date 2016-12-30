30e9fac66ed93a9d7a42ceaf68382455b58ff01f
Marco <marcogaido91@gmail.com>
2016-12-30 09:27:20 +0100

    Rnaming SQLContext methods

--- src/main/scala-2.10/it/ecubecenter/spark/hbase/package.scala
+++ src/main/scala-2.10/it/ecubecenter/spark/hbase/package.scala
@@ -9,11 +9,11 @@ import org.apache.spark.sql.types.DataType
   */
 package object hbase {
   implicit class HBaseEnhancedSQLContext(sqlContext:SQLContext) {
-    def delete(tableName:String, filterFunction: RowkeyFilterFunction) = BulkDelete.delete(sqlContext, tableName, filterFunction)
-    def delete(sqlContext: SQLContext, tableName:String, filterFunction: RowkeyFilterFunction, additionalColumn:Iterable[HBaseColumn]) = BulkDelete.delete(sqlContext, tableName, filterFunction, additionalColumn)
-    def delete(sqlContext: SQLContext, namespace:String, tableName:String, filterFunction: RowkeyFilterFunction) = BulkDelete.delete(sqlContext, namespace, tableName, filterFunction)
-    def delete(sqlContext: SQLContext, tableName:String, filterFunction: RowkeyFilterFunction, rowkeyType:DataType) = BulkDelete.delete(sqlContext, tableName, filterFunction, rowkeyType)
-    def delete(sqlContext: SQLContext, namespace:String, tableName:String, filterFunction: RowkeyFilterFunction, rowkeyType:DataType, additionalColumns:Iterable[HBaseColumn]) =
+    def hbaseBulkdelete(tableName:String, filterFunction: RowkeyFilterFunction) = BulkDelete.delete(sqlContext, tableName, filterFunction)
+    def hbaseBulkdelete(sqlContext: SQLContext, tableName:String, filterFunction: RowkeyFilterFunction, additionalColumn:Iterable[HBaseColumn]) = BulkDelete.delete(sqlContext, tableName, filterFunction, additionalColumn)
+    def hbaseBulkdelete(sqlContext: SQLContext, namespace:String, tableName:String, filterFunction: RowkeyFilterFunction) = BulkDelete.delete(sqlContext, namespace, tableName, filterFunction)
+    def hbaseBulkdelete(sqlContext: SQLContext, tableName:String, filterFunction: RowkeyFilterFunction, rowkeyType:DataType) = BulkDelete.delete(sqlContext, tableName, filterFunction, rowkeyType)
+    def hbaseBulkdelete(sqlContext: SQLContext, namespace:String, tableName:String, filterFunction: RowkeyFilterFunction, rowkeyType:DataType, additionalColumns:Iterable[HBaseColumn]) =
       BulkDelete.delete(sqlContext, namespace, tableName, filterFunction, rowkeyType, additionalColumns)
   }
 }
